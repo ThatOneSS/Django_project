ff4e16327950ad8077c500370b099eb18203d7be
ThatOneSS <saidakbarsamadov1@gmail.com>
2025-05-19 01:38:08 +0500

    Add Render URL to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ass3ax8g3g_=5d8rvbbj06kky6sjkmld!2at0x3bb5=gq-#(dr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['notavtoelonuz.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
